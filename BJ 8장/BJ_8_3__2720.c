f3cd6e5ddd187ed2e6176e665d30bd08a51b91d9
sentiaix <knnxcs@gmail.com>
2025-04-30 17:21:05 +0900

    Feat: 8장 3번 다시 확인하기. 마지막 노트북에서 커밋함

--- BJ 8장/BJ_8_3__2720.c
+++ BJ 8장/BJ_8_3__2720.c
@@ -5,14 +5,20 @@
 // <입략> : 1. 케이스 수, 2. 케이스 수 만큼의 N센트 입력
 // 메모리 소수점 연산 오류를 막기 위해 소수점연산은 하지 않음.
 
-void Mmod(int money, int* q, int* d, int* n, int* p) {
+// 컴파일에서 확인하는 순서와 실제 실행중 메모리 참조 순서는 다름.
+// 함수는 호출했을때 반응함.
+// 순서: main함수 > main함수 내부에서 호출한 순서대로 함수가 작동(지역변수생성 등)함.
+
+void Mmod(int money, int* q, int* d, int* n, int* p){
+    // 4. main에서 호출된 값을 받아 main에 반환함
+
     *q = money / 25; money %= 25; // Quarter: 25c
     *d = money / 10; money %= 10; // Dime: 10c
     *n = money / 5;  money %= 5; // Nickel: 5c
     *p = money; // 나머지 = Penny의 수 // Penny: 1c
 }
 
-int main() {
+int main() { // 1. 메인함수가 호출됨
     int n;
     scanf("%d", &n);
 
@@ -22,8 +28,8 @@ int main() {
     }
 
     for(int i = 0; i < n; i++){
-        int q, d, n, p;
-        Mmod(arr[i], &q, &d, &n, &p);
+        int q, d, n, p; // 2. qdnp 변수들을 선언함.
+        Mmod(arr[i], &q, &d, &n, &p); // 3. Mmod를 호출하며 qdnp의 주소를 제공함.
         printf("%d %d %d %d\n", q, d, n, p);
     } 
 
