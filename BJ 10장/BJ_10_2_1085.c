090c3fa3637b3c4e0e64b753fa8d86062cb3e90e
sentiaix <knnxcs@gmail.com>
2025-06-02 16:12:36 +0900

    Feat: 10-2 easy.

--- BJ 10장/BJ_10_2_1085.c
+++ BJ 10장/BJ_10_2_1085.c
@@ -5,39 +5,16 @@
 // 직사각형은 좌하단 꼭짓점이 (0,0), 우상단 꼭짓점이 (w,h)이고
 // 문제점1: 왼쪽 아래랑도 거리 확인해야함
 
-// define struct
-struct distance{
-    int x;
-    int y;
-    int w;
-    int h;
-};
-
-// function declaration
-void fnd_dist(struct distance* p);
-struct distance scanf_v();
-
 int main(){
-    struct distance a;
+    int x,y,w,h;
 
-    a = scanf_v(a.x, a.y, a.w, a.h);
-    fnd_dist( &a );
+    scanf("%d %d %d %d", &x, &y, &w, &h);
+
+    int near_x = (w - x) < x ? (w - x) : x; // (오른쪽 벽과의 거리) < (왼쪽 벽과의 거리)
+    int near_y = (h - y) < y ? (h - y) : y; // 저런 식으로 비교하여, 조건식이 참이면 (( -? A:B ))중 A를 반환.
+    int result = near_x < near_y ? near_x : near_y;
+
+    printf("%d\n", result);
 
     return 0;
-}
-
-// define function
-
-// 거리 판독 함수
-void fnd_dist(struct distance* p){
-    int left, right, top, bottom; // 현재 기준에서 왼쪽과의 거리, 오른쪽과의 거리 등..
-}
-
-// 변수 받는 함수
-struct distance scanf_v(){
-    struct distance tmp;
-
-    scanf("%d %d %d %d", tmp.x, tmp.y, tmp.w, tmp.h);
-
-    return tmp;
 }
