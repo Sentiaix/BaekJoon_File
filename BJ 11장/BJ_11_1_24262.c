505b068c0bedd9bcf241645e71531418e2fa02d7
sentiaix <knnxcs@gmail.com>
2025-06-04 10:04:42 +0900

    Feat: 난 얘가 뭘 원하는지 모르겠다

---
+++ BJ 11장/BJ_11_1_24262.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+// <문제>: https://www.acmicpc.net/problem/24262
+
+// 난 이 문제가 뭘 원하는지 모르겠다
+
+int main(){
+    printf("1\n0\n");
+    return 0;
+}
