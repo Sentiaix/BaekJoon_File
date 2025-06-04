4e7181292b928332cbd78c855ab2b5f6a5266737
sentiaix <knnxcs@gmail.com>
2025-06-04 10:05:35 +0900

    Feat: 어느정도 뭘 요구하는지 알겠다

---
+++ BJ 11장/BJ_11_2_24263.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+// <문제>: https://www.acmicpc.net/problem/24263
+
+// 의사코드를 보고 그 함수가 실행될 출력값을 출력시키는 코드를 짜라고 하는듯
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    printf("%d\n1\n", n);
+
+    return 0;
+}
