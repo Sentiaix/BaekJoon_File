c6816d48aa10c53b8ffd71c91df97e69ba1259f6
sentiaix <knnxcs@gmail.com>
2025-06-05 17:23:12 +0900

    Feat: 아직 미완료료

---
+++ BJ 11장/BJ_11_7_24313.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+// <문제>: https://www.acmicpc.net/problem/24313
+
+int main(){
+    int f_1, f_c; //f_1 > 함수 f에 속하는 1차항의 계수, f_c > 함수 f에 속하는 상수항    
+    int c; // 비교군에 속하는 상수항
+    int min_n; // n값중 가장 작은 n0값
+    // O(g(n)) = {f(n) | 모든 n ≥ n0에 대하여 f(n) ≤ c × g(n)인 양의 상수 c와 n0가 존재한다}
+
+    
+
+    return 0;
+}
