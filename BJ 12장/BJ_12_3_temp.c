52bb611aaeeb284ea9fd6364f111c02c6656eaf4
sentiaix <knnxcs@gmail.com>
2025-09-19 15:49:05 +0900

    Feat: 아직도 틀려있음

--- BJ 12장/BJ_12_3_temp.c
+++ BJ 12장/BJ_12_3_temp.c
@@ -9,7 +9,7 @@ enum {x,y,c}; // x == 0, y == 1, c == 2
 
 // typedef struct Coefficient {
 //     int row_1[3]; // 첫번째 행
-//     int row_2[3]; // 두번째 행
+//     int row_2[3]; // 두번째 행 
 // } HeadNumber;
 
 // function declaration
@@ -26,7 +26,7 @@ int main(){
     // 값 받기 (x1 y1 c1, x2 y2 c2)
     for(int i = 0; i < 2; i++){
         for(int j = 0; j < 3; j++){
-            scanf("%f", &row[i][j]);
+            scanf("%d", &row[i][j]);
         }
     }
 
@@ -53,11 +53,29 @@ int main(){
     temp[0] = row[0][y];
     temp[1] = row[1][y];
 
-    for(int i = 0; i < 3; i++){
-        row[0][i] *= temp[1 - i];
+    // y1 y2끼리 서로 식의 계수들을 곱함
+    for(int j = 1; j > -1; j--){
+        for(int i = 0; i < 3; i++){
+            row[0][i] *= temp[j];
+        }
     }
+    // 여기까지 했으면
+    // 0x + ay = C1
+    // nx + ay = C2 꼴
 
+    // 1번째 행은 y만, 2번째 행은 x만 남음
+    for(int i = 0; i < 3; i++){
+        row[1][i] -= row[0][i];
+    }
     
+    if (row[1][x] == 0 || row[0][y] == 0) {
+        // x 또는 y의 계수가 0이 되는 경우는 문제 조건상 없음
+        return 1; // 비정상 종료
+    }
+    int ans_x = row[1][c] / row[1][x];
+    int ans_y = row[0][c] / row[0][y];
+
+    printf("%d %d\n", ans_x, ans_y);
 
     return 0;
 }
